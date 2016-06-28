3f8c3bfe164780917ab57cb398cdbd61167dd7d6
Riley Pacella <rpacella@umich.edu>
2016-06-28 22:12:32 +0200

    finished 2B

--- o_X/BoardViewController.swift
+++ o_X/BoardViewController.swift
@@ -6,12 +6,16 @@
 import UIKit
 
 class BoardViewController: UIViewController {
-
+    
+    
     @IBOutlet weak var newGameButton: UIButton!
-    // Create additional IBOutlets here.
+    
+    @IBOutlet weak var boardView: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -21,6 +25,37 @@ class BoardViewController: UIViewController {
     }
     
     // Create additional IBActions here.
+    
+    @IBAction func cellButtonPressed(sender: AnyObject) {
+        
+        switch sender.tag {
+        case 1:
+            print("button 1 pressed")
+        case 2:
+            print("button 2 pressed")
+        case 3:
+            print("button 3 pressed")
+        case 4:
+            print("button 4 pressed")
+        case 5:
+            print("button 5 pressed")
+        case 6:
+            print("button 6 pressed")
+        case 7:
+            print("button 7 pressed")
+        case 8:
+            print("button 8 pressed")
+        case 9:
+            print("button 9 pressed")
+        default:
+            print("default")
 
+        }
+    }
+    
+    @IBAction func logOutButtonPressed(sender: AnyObject) {
+        print("logout button pressed")
+    }
+    
+    
 }
-
